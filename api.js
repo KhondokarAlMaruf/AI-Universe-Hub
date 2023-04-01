b1ca6be5fe4ffb61d4c89f68e4727d1d39729149
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-04-02 04:51:43 +0600

    inner html and card design added

--- api.js
+++ api.js
@@ -8,11 +8,34 @@ const loadData = async () => {
 const displayData = (univers) => {
   console.log(univers);
   const universeContainer = document.getElementById("universe-container");
+
   univers.forEach((univer) => {
     console.log(univer.image);
     const universDiv = document.createElement("div");
     universDiv.classList.add("col");
-
+    universDiv.innerHTML = `<div class="card h-100 p-3">
+                            <img src="${univer.image}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Features</h5>
+                                ${univer.features}
+                            </div>
+                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                <div class="">
+                                <div class="fs-3">
+                                ${univer.name}
+                                </div>
+                                <div>
+                                <i class="fa-regular fa-calendar-days"></i> ${univer.published_in}
+                                </div>
+                                </div>
+                                <div class="">
+                                <button><i class="px-2 fa-solid fa-arrow-right"></i></button>
+                                </div>
+                                
+                                
+                            </div>
+                        </div>
+    `;
     universeContainer.appendChild(universDiv);
   });
 };
