5b981a0a52dbb709e189b89c7eabb1a2b69ee87a
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-04-02 04:35:15 +0600

    fetch added

---
+++ api.js
@@ -0,0 +1,8 @@
+const loadData = async () => {
+  const url = ` https://openapi.programming-hero.com/api/ai/tools`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayData(data.data.tools);
+};
+
+loadData();
